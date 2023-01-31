8eb7ba8c7463548c8ed724f0c6328cad36a638d2
PeerHerholz <herholz.peer@gmail.com>
2023-01-31 12:32:57 +0100

    udpate path to metadata templates

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     include_package_data=True,
     # package_dir={"": 'data'},
     package_data={
-        '': ['data/metadata_templates/*.json'
+        '': ['data/atlas_metadata/*.json'
              # When adding files here, remember to update MANIFEST.in as well,
              # or else they will not be included in the distribution on PyPI!
              # 'path/to/data_file',
