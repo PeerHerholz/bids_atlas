3b895d4cc30d684fbef02b7c984e0737479631ea
PeerHerholz <PeerHerholz@users.noreply.github.com>
2023-02-14 16:09:48 +0000

    add logo efeeb94b150b1e4eaf06402c42186c33a8cc1808

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gb0b409c',
+    VERSION: '0.post1+gefeeb94',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
